0344fe1abf2940dedd8ac856cb68d835acba816a
FranVasEstay <fran.vasquez.e@gmail.com>
2026-06-02 17:13:45 -0400

    Reorder Descriptive script

---
+++ Scripts/03_Descriptivo_v1.R
@@ -0,0 +1,7 @@
+# =============================================================================
+# TESIS: Mega incendio Viña del Mar-Valparaíso 2024 y consultas de urgencia
+# Diseño: Series de tiempo interrumpidas (ITS) con regresión Binomial Negativa
+# Repositorio: [GitHub - Enlace a tu repositorio público]
+# =============================================================================
+
+
